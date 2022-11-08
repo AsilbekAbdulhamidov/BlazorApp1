ddbcf912a4fc3821b89218151e5317a06810f2b4
AsilbekAbdulhamidov <104564092+AsilbekAbdulhamidov@users.noreply.github.com>
2022-11-08 20:04:31 +0500

    Add project files.

---
+++ BlazorApp1/Data/People.cs
@@ -0,0 +1,10 @@
+namespace BlazorApp1.Data
+{
+    public class People
+    {
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public string Email { get; set; }   
+        public int Age { get; set; }
+    }
+}
